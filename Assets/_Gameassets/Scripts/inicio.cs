55d5833ca56e7b616beff705ac561681b4ae9e6d
Desarrollo de videojuegos con Unity <wxivyc@gmail.com>
2024-04-08 20:11:59 +0200

    Ajuste

--- Assets/_Gameassets/Scripts/inicio.cs
+++ Assets/_Gameassets/Scripts/inicio.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneScript : MonoBehaviour
+public class inicio : MonoBehaviour
 {
     
     public void IniciarJuego(){
