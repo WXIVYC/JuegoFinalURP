9632031d3b4eb6224ef927713ebf8e506126cc6d
Desarrollo de videojuegos con Unity <wxivyc@gmail.com>
2024-04-16 20:31:30 +0200

    Script Marea

--- Assets/_Gameassets/Scripts/PatrullajeManagerAvanzado.cs
+++ Assets/_Gameassets/Scripts/PatrullajeManagerAvanzado.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PatrullajeManager : MonoBehaviour
+public class PatrullajeManagerAvanzado : MonoBehaviour
 {
     public List<Transform> targets;
     public bool patrullajeAleatorio = true;
 
     [Range(0,10)]
     public float tiempoEspera; 
+
+    [Header("A partir de esta distancia deja de patrullar y sigue al player")]
+    public float distanciaSeguimientoDirecto;
+    public float distanciaAlPlayer;
+    public string nombrePlayer = "Player";
     private int nextTarget = 0;
     private NavMeshAgent navMeshAgent;
 
+    private GameObject player;
+
     private bool esperandoAsignacion=false;
+
     void Start()
     {
+        player = GameObject.Find(nombrePlayer);
+        if (player == null){
+            Debug.LogError("No se ha encontrado al player en el PatrullajeManagerAvanzado");
+        }
         navMeshAgent = GetComponent<NavMeshAgent>();
         if (patrullajeAleatorio)
         {
@@ -25,6 +37,7 @@ public class PatrullajeManager : MonoBehaviour
     }
     void Update()
     {
+        distanciaAlPlayer=(player.transform.position - transform.position).magnitude;
         DeterminarSiguienteTarget();
     }
     private void DeterminarSiguienteTarget()
@@ -32,6 +45,9 @@ public class PatrullajeManager : MonoBehaviour
         if (esperandoAsignacion) return;
         if (!navMeshAgent.pathPending)
         {
+            if (distanciaAlPlayer<=distanciaSeguimientoDirecto){
+                AsignarTargetPlayer();
+            }
             if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
             {
                 if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
@@ -62,5 +78,9 @@ public class PatrullajeManager : MonoBehaviour
         nextTarget++;
         if (nextTarget == targets.Count) nextTarget = 0;
         navMeshAgent.destination = targets[nextTarget].position;
+    }
+    private void AsignarTargetPlayer(){
+        esperandoAsignacion=false;
+        navMeshAgent.destination = player.transform.position;
     }
 }
